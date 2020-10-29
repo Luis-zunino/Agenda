f365b729f8e508c2a00e370c5aaace16b5acc583
Luis Zunino <zuninoluis01@gmail.com>
2020-10-29 12:27:16 -0300

    Initial commit

---
+++ INC/layout/header.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<header>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale = 1.0 ">
+    <meta  http-equiv = " X-UA-Compatible "  content = " ie = edge " >
+    <título > Agenda de Contactos </ título >
+    <link  rel = " stylesheet "  href = " https://necolas.github.io/normalize.css/8.0.0/normalize.css " >
+    <Enlace  rel = " hoja de estilo "  href = " https://use.fontawesome.com/releases/v5.4.1/css/all.css "  integridad = " sha384-5sAR7xN1Nv6T6 + dT2mhtzEpVJvfS3NScPQTrOxhwjIuvcA67KV2R5Jz6kr4abQsz "  crossorigin = " anónimo " >
+    <link  rel = " stylesheet "  href = "css/styles.css " >
+</header>
+<body>
---
+++ index.php
@@ -0,0 +1,33 @@
+<?php include "inc/layout/header.php"; ?>
+
+
+
+<div class="contenedor-barra">
+    <h1>Agenda de Contactos</h1>
+</div>
+<div class="bg-amarillo contenedor sombra">
+    <form action="#" id="contacto">
+        <legend>Añada un campo <span>Todos los campos son obligatorios</span></legend>
+    <div class="campos">
+        <div class="campo">
+        <label for="nombre">Nombre:</label>
+        <input type="text" placeholder="Nombre Contacto" id="nombre">
+        </div>
+        <div class="campo">
+        <label for="empresa">Empresa:</label>
+        <input type="text" placeholder="Nombre Empresa" id="empresa">
+        </div>
+        <div class="campo">
+        <label for="nombre">Teléfono:</label>
+        <input type="tel" placeholder="Teléfono" id="nombre">
+        </div>
+        <div class="campo enviar">
+            <input type="submit" value="Añadir">
+        </div>
+    </div>
+    </form>
+</div>
+
+
+
+<?php include "inc/layout/footer.php"; ?>
