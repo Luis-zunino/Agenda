f927e5c1984c621900f74bf790372b930725e956
Luis Zunino <zuninoluis01@gmail.com>
2020-10-30 17:42:25 -0300

    styles.css
    se agregan estilos responsivos

--- index.php
+++ index.php
@@ -7,24 +7,24 @@
 </div>
 <div class="bg-amarillo contenedor sombra">
     <form action="#" id="contacto">
-        <legend>Añada un campo <span>Todos los campos son obligatorios</span></legend>
-    <div class="campos">
-        <div class="campo">
-        <label for="nombre">Nombre:</label>
-        <input type="text" placeholder="Nombre Contacto" id="nombre">
-        </div>
-        <div class="campo">
-        <label for="empresa">Empresa:</label>
-        <input type="text" placeholder="Nombre Empresa" id="empresa">
-        </div>
-        <div class="campo">
-        <label for="nombre">Teléfono:</label>
-        <input type="tel" placeholder="Teléfono" id="nombre">
+        <legend class="cen">Añada un campo <span>Todos los campos son obligatorios</span></legend>
+        <div class="campos">
+            <div class="campo">
+                <label for="nombre">Nombre:</label>
+                <input type="text" placeholder="Nombre Contacto" id="nombre">
+            </div>
+            <div class="campo">
+                <label for="empresa">Empresa:</label>
+                <input type="text" placeholder="Nombre Empresa" id="empresa">
+            </div>
+            <div class="campo">
+                <label for="nombre">Teléfono:</label>
+                <input type="tel" placeholder="Teléfono" id="nombre">
+            </div>
         </div>
         <div class="campo enviar">
             <input type="submit" value="Añadir">
         </div>
-    </div>
     </form>
 </div>
 
