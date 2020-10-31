584222136662c0afdaacc4356c4d441cb2c3c2ed
Luis Zunino <zuninoluis01@gmail.com>
2020-10-30 22:06:54 -0300

    app.js
    se crea el boton flotante

--- JS/app.js
+++ JS/app.js
@@ -15,9 +15,30 @@ function leerFormulario(e) {
         empresa = document.querySelector("#empresa").value,
         telefono = document.querySelector("#telefono").value;
     if (nombre === "" || empresa === "" || telefono === "") {
-        console.log("Los campos esta vacio")
+        //DOS PARAMETROS TEXTO Y CLASE
+        mostrarNotificacion("Todos los campos son obligatorios", "error");
     } else {
         console.log("tiene algo");
     }
     console.log(nombre);
+}
+
+//NOTIFICACIONES EN PANTALLA
+function mostrarNotificacion(mensaje, clase) {
+    const notificacion = document.createElement("div");
+    notificacion.classList.add(clase, "notificacion", "sombra");
+    notificacion.textContent = mensaje;
+
+    //formulario
+    formularioContactos.insertBefore(notificacion, document.querySelector("form legend"));
+    //ocultar y mostrar la notificacion
+    setTimeout(() => {
+        notificacion.classList.add("visible");
+        setTimeout(() => {
+            notificacion.classList.remove("visible");
+            setTimeout(() => {
+                notificacion.remove();
+            }, 500)
+        }, 3000)
+    }, 100)
 }
