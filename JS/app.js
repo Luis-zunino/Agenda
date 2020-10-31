4a366739275a76d2dddcae70a8020c2eb9d298ad
Luis Zunino <zuninoluis01@gmail.com>
2020-10-31 00:37:15 -0300

    app.js
    se hace la conecion con ajax

--- JS/app.js
+++ JS/app.js
@@ -13,16 +13,48 @@ function leerFormulario(e) {
     //leero los datos de los imputs
     const nombre = document.querySelector("#nombre").value,
         empresa = document.querySelector("#empresa").value,
-        telefono = document.querySelector("#telefono").value;
+        telefono = document.querySelector("#telefono").value,
+        accion = document.querySelector("#accion").value;
+
     if (nombre === "" || empresa === "" || telefono === "") {
         //DOS PARAMETROS TEXTO Y CLASE
         mostrarNotificacion("Todos los campos son obligatorios", "error");
     } else {
-        console.log("tiene algo");
+        //PASA LA VALIDACION, CREAR LLAMADO A AJAX
+        const infoContacto = new FormData();
+        infoContacto.append("nombre", nombre);
+        infoContacto.append("empresa", empresa);
+        infoContacto.append("telefono", telefono);
+        infoContacto.append("accion", accion);
+
+        //console.log(...infoContacto);
+        if (accion === "crear") {
+            //creamos un nuevo contacto
+            insertarBD(infoContacto);
+        } else {
+            //editar el contacto
+        }
+
     }
     console.log(nombre);
 }
+/**INSERTA EN LA BASE DE DATOS VIA AJAX **/
+function insertarBD(datos){
+//llamado a ajax
 
+//crear el objeto
+const xhr = new XMLHttpRequest();
+//abrir conexion
+xhr.open("POST", "inc/modelos/modelo-contactos.php", true);
+//pasar los datos
+xhr.onload=function(){
+    if(this.status===200){
+        console.log(JSON.parse(xhr.responseText) );
+    }
+}
+//enviar los datos
+xhr.send(datos)
+}
 //NOTIFICACIONES EN PANTALLA
 function mostrarNotificacion(mensaje, clase) {
     const notificacion = document.createElement("div");
