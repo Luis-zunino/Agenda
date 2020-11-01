efa9f7099ffcca29823beb1e66a6e981947d3d20
Luis Zunino <zuninoluis01@gmail.com>
2020-11-01 16:26:35 -0300

    Update app.js
    se muestra el msj de agregado correctamente el contacto y ademas se recetea el formulario para ingresar datos

--- JS/app.js
+++ JS/app.js
@@ -1,54 +1,55 @@
 const formularioContactos = document.querySelector('#contacto'),
-      listadoContactos = document.querySelector('#listado-contactos tbody');
- 
+    listadoContactos = document.querySelector('#listado-contactos tbody');
+
 eventListeners();
- 
+
 function eventListeners() {
     formularioContactos.addEventListener('submit', leerFormulario);
 }
+
 function leerFormulario(e) { //e es el evento
     e.preventDefault(); //Evita cambiar la url con la accion, recomend for java/ajax
     const nombre = document.querySelector('#nombre').value, //captura valor introducido en id
-        telefono = document.querySelector('#telefono').value,   // en los inputs
+        telefono = document.querySelector('#telefono').value, // en los inputs
         empresa = document.querySelector('#empresa').value,
         accion = document.querySelector('#accion').value;
-    if(nombre === '' || empresa === '' || telefono ==='') {
+    if (nombre === '' || empresa === '' || telefono === '') {
         //dos parametros
         mostrarNotificacion('All fields must be field', 'error');
     } else {
- 
+
         // Pasa la validacion, crear llamado a Ajax
         const infoContacto = new FormData();
         infoContacto.append('nombre', nombre);
         infoContacto.append('empresa', empresa);
         infoContacto.append('telefono', telefono);
         infoContacto.append('accion', accion);
- 
+
         //console.log(...infoContacto); Permite leer los datos enviados
-        if(accion === 'crear'){
+        if (accion === 'crear') {
             // crearemos un nuevo contacto
             insertarBD(infoContacto);
         } else {
- 
+
         }
     }
 }
 /* INSERTAR MEDIANTE AJAX */
 function insertarBD(datos) {
     // llamado a ajax
- 
+
     // crear el objeto
     const xhr = new XMLHttpRequest();
- 
+
     // abrir la conexion
     xhr.open('POST', 'inc/modelos/modelo-contactos.php', true);
- 
+
     // pasar los datos
-    xhr.onload = function() {
-        if(this.status === 200) {
-            console.log(JSON.parse( xhr.responseText) );//JSON.parse permite acceder facil a sus datos
+    xhr.onload = function () {
+        if (this.status === 200) {
+            console.log(JSON.parse(xhr.responseText)); //JSON.parse permite acceder facil a sus datos
             // leemos la respuesta de PHP
-            const respuesta = JSON.parse( xhr.responseText);
+            const respuesta = JSON.parse(xhr.responseText);
             //Insertar nuevo elemento en tabla
             const nuevoContacto = document.createElement('tr');
             nuevoContacto.innerHTML = `
@@ -59,35 +60,39 @@ function insertarBD(datos) {
             //crear contenedores botones
             const contenedorAcciones = document.createElement('td');
             //Icono editar
-            const iconoEditar = document.createElement('i');//crea un elemento i
-            iconoEditar.classList.add('fas', 'fa-pen-square');//le agrega clases al icono creado
+            const iconoEditar = document.createElement('i'); //crea un icono editar ( i )
+            iconoEditar.classList.add('fas', 'fa-pen-square'); //le agrega clases al icono creado
             //crear el enlace para editar
-            const btnEditar = document.createElement('a');//crea un lemento a
-            btnEditar.appendChild(iconoEditar);// le agrega como hijo al elemento i creado anteriormente (a padre / i hijo)
-            btnEditar.href = `editar.php?id=${respuesta.datos.id_insertado}`;//le agrega el id insertar_datos que se encuentra en editar.php( la respuesta biene de modelo-contactos.php)
-            btnEditar.classList.add('btn', 'btn-editar');//le agrega las clases
+            const btnEditar = document.createElement('a'); //crea un lemento a
+            btnEditar.appendChild(iconoEditar); // le agrega como hijo al elemento i creado anteriormente (a padre / i hijo)
+            btnEditar.href = `editar.php?id=${respuesta.datos.id_insertado}`; //le agrega el id insertar_datos que se encuentra en editar.php( la respuesta biene de modelo-contactos.php)
+            btnEditar.classList.add('btn', 'btn-editar'); //le agrega las clases
             //agregarlo al padre
-            contenedorAcciones.appendChild(btnEditar);// le agrega como hijo el elemento a que a su vez tiene como hijo el elemento i.
+            contenedorAcciones.appendChild(btnEditar); // le agrega como hijo el elemento a que a su vez tiene como hijo el elemento i.
             //icono eliminar
-            const iconoEliminar = document.createElement('i');//crea el icono no el boton de eliminar
-            iconoEliminar.classList.add('fas', 'fa-trash-alt');//le agrega las clases
+            const iconoEliminar = document.createElement('i'); //crea el icono no el boton de eliminar
+            iconoEliminar.classList.add('fas', 'fa-trash-alt'); //le agrega las clases
             //btn eliminar
-            const btnEliminar = document.createElement("button");//crea el boton de eliminar
-            btnEliminar.appendChild(iconoEliminar);
-            btnEliminar.setAttribute('data-id', respuesta.datos.id_insertado);
-            btnEliminar.classList.add('btn', 'btn-borrar');
-            ///agregarlo al daddy
+            const btnEliminar = document.createElement("button"); //crea el boton de eliminar
+            btnEliminar.appendChild(iconoEliminar); //le agrega como hijo el icono i creado anteriormente (iconoEliminar)
+            btnEliminar.setAttribute('data-id', respuesta.datos.id_insertado); // cada boton eliminar tiene un data-id por eso le agrega ese atributo para luego poder eliminarlo de la base de datos el cual viene de respuesta.datos.id_insertado
+            btnEliminar.classList.add('btn', 'btn-borrar'); //le agrega las clases
+            ///agregarlo al padre
             contenedorAcciones.appendChild(btnEliminar);
             //agregar al tr
             nuevoContacto.appendChild(contenedorAcciones);
-            //agregarlo con los contacts
+            //agregarlo con los contactos
             listadoContactos.appendChild(nuevoContacto);
+            //resetear el formulario
+            document.querySelector("form").reset();
+            //mostrar la notificacion
+            mostrarNotificacion("Contacto Creado Correctamente", "correcto")
         }
     };
     //enviar datos
     xhr.send(datos)
 }
- 
+
 //Notficacion de pantalla
 function mostrarNotificacion(mensaje, clase) {
     const notification = document.createElement('div'); //Crea un div donde se introduce el elemento
@@ -96,9 +101,9 @@ function mostrarNotificacion(mensaje, clase) {
     //Inserta el mensaje antes del form.
     formularioContactos.insertBefore(notification, document.querySelector('form legend'));
     // Ocultar y Mostrar notificacion
-    setTimeout(() => {  //Actua al cabo de 0.1s
+    setTimeout(() => { //Actua al cabo de 0.1s
         notification.classList.add('visible'); //Anade clase sobre notificacion
-        setTimeout(() => {  //quita la clase al cabo de 3s
+        setTimeout(() => { //quita la clase al cabo de 3s
             notification.classList.remove('visible');
             setTimeout(() => {
                 notification.remove(); //espera medio segundo para que se vea la animacion
