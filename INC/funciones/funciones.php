a6ec40bbeaba003ab5eae00c304e67b68595b3ad
Luis Zunino <zuninoluis01@gmail.com>
2020-11-01 18:30:06 -0300

    funciones.php
    funcion que hace que la parte de contactos sea mas dinamica

---
+++ INC/funciones/funciones.php
@@ -0,0 +1,15 @@
+<?php
+//funcion que va a mostrar los registros de la base de datos en la parte inferior ("2 contactos")
+function obtenerContactos(){
+    //cuando mande a ejecutar la funcion o sea la mande llamar en los scripts va a incluir una coneccion para poder realizar una consulta
+    include "bd.php";
+    try {
+        //va a realizar una consula a un query a lo que viene siendo la base de datos( con el mismo nombre que se creo la conexion en este caso conn)
+    return $conn->query(" SELECT id, nombre, telefono FROM contactos" /*se coloca la instruccion a SQL*/);
+    } catch (Exception $e) {
+        //para que retorne un mensaje de error
+        echo "Error!" . $e->getMessage() . "<br>";
+        //retonra false cada vez que haya un error asi de este modo se puede colocar un if y cada vez que se ejecuta la funcion no haga nada
+        return false;
+    }
+}
