be25982864174ad63c8275c17a225ed6c3e94dec
Luis Zunino <zuninoluis01@gmail.com>
2020-11-06 21:31:43 -0300

    Update formulario.php
    se agrega la opcion de que cuando se este editando aparezcan los datos y si no que no aparezcan los datos

--- INC/layout/formulario.php
+++ INC/layout/formulario.php
@@ -1,15 +1,32 @@
 <div class="campos">
     <div class="campo">
         <label for="nombre">Nombre:</label>
-        <input type="text" placeholder="Nombre Contacto" id="nombre">
+        <input 
+        type="text" 
+        placeholder="Nombre Contacto" 
+        id="nombre"
+        value=" <?php echo (isset($contacto["nombre"])) ? $contacto["nombre"] : ""; //comprobar que si esta
+        //variable contacto exite entonces se editara el contacto, si no existe se estara 
+        //creando uno, funciona como si fuera un if ?>"
+        >
     </div>
     <div class="campo">
         <label for="empresa">Empresa:</label>
-        <input type="text" placeholder="Nombre Empresa" id="empresa">
+        <input 
+        type="text" 
+        placeholder="Nombre Empresa" 
+        id="empresa"
+        value="<?php echo (isset($contacto["empresa"])) ? $contacto["empresa"] : ""; ?>"
+        >
     </div>
     <div class="campo">
         <label for="telefono">Teléfono:</label>
-        <input type="tel" placeholder="Teléfono de Contacto" id="telefono">
+        <input 
+        type="tel" 
+        placeholder="Teléfono de Contacto" 
+        id="telefono"
+        value=" <?php echo (isset($contacto["telefono"])) ? $contacto["telefono"] : ""; ?>"
+        >
     </div>
 </div>
 <div class="campo enviar">
